5f9079733219e28726d35b1e36c1de58e61267db
zdk <nx2zdk@gmail.com>
2014-08-24 21:45:55 +0700

    Change test text in test.sql

--- thai_parser/sql/test.sql
+++ thai_parser/sql/test.sql
@@ -3,9 +3,9 @@ DROP TEXT SEARCH CONFIGURATION testthaicfg;
 CREATE TEXT SEARCH CONFIGURATION testthaicfg (PARSER = thai_parser);
 ALTER TEXT SEARCH CONFIGURATION testthaicfg ADD MAPPING FOR a WITH simple;
 
-select * from ts_parse('thai_parser', 'ภาษาไทยเป็นภาษาที่ง่ายที่สุดในโลก');
-select to_tsvector('testthaicfg', 'ภาษาไทยเป็นภาษาที่ง่ายที่สุดในโลก');
-SELECT to_tsquery('testthaicfg', 'ภาษาไทยเป็นภาษาที่ง่ายที่สุดในโลก');
+select * from ts_parse('thai_parser', 'สวัสดีครับทดสอบภาษาไทย');
+select to_tsvector('testthaicfg', 'สวัสดีครับทดสอบภาษาไทย');
+SELECT to_tsquery('testthaicfg', 'สวัสดีครับทดสอบภาษาไทย');
 
-select 'ERROR ts_parse for ภาษาไทยเป็นภาษาที่ง่ายที่สุดในโลก' where 
-    (select count(*) from ts_parse('thai_parser', 'ภาษาไทยเป็นภาษาที่ง่ายที่สุดในโลก'))<>9;
+select 'ERROR ts_parse for สวัสดีครับทดสอบภาษาไทย' where
+    (select count(*) from ts_parse('thai_parser', 'สวัสดีครับทดสอบภาษาไทย'))<>9;
